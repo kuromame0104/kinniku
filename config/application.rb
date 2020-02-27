cba1b10db273d052f8ff305c8f2ca6388fbc47b1
kuromame0104 <newgene4865@icloud.com>
2020-02-27 14:53:55 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Kinniku
+module foodshare
   class Application < Rails::Application
     config.generators do |g|
       g.stylesheets false
