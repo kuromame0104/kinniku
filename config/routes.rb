5e5fe9cd58df84b4c742b8f575d6a1557b5bd986
kuromame0104 <newgene4865@icloud.com>
2020-02-28 15:30:57 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'messages#index'
+  devise_for :users
   resources :messages, only: [:index, :new, :create, :edit, :destroy, :show, :update] do
     resources :comments, only: [:create]
   end
