5ce761e8236820982b18b909f63070088b28115c
kuromame0104 <newgene4865@icloud.com>
2020-02-28 18:21:54 +0900

    S3修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :comments, dependent: :destroy
+  mount_uploader :image, ImageUploader
 
   validates :text, presence: true
   # mount_uploader :image, ImageUploader
